60a91d378da562d7c3c91399d109bdc59e7634bb
SakinaKarimi <sakinatuba145@gmail.com>
2026-05-17 22:34:06 +0430

    add api services

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
+import 'package:education_app/features/login_screen.dart';
 import 'package:flutter/material.dart';
-import 'profile/profile_screen.dart';
-
 void main() {
   runApp(const MyApp());
 }
@@ -12,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const ProfileScreen(),
+      home: const LoginScreen(),
     );
   }
 }
