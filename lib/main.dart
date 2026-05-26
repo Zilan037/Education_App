94e03084404ca467b9e8ed82d96eaf83463f30be
SakinaKarimi <sakinatuba145@gmail.com>
2026-05-26 08:05:03 +0430

    flutter project

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,72 @@
-import 'package:education_app/features/login_screen.dart';
 import 'package:flutter/material.dart';
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'core/helpers/shared_preferences_helper.dart';
+import 'features/login_screen.dart';
+import 'firebase_options.dart';
+import 'wrapper.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options:
+    DefaultFirebaseOptions.currentPlatform,
+  );
+
+  await SharedPreferencesHelper.init();
+
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() =>
+      _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  bool isDark = false;
+
+  void toggleTheme() {
+
+    setState(() {
+
+      isDark = !isDark;
+
+      SharedPreferencesHelper
+          .setBool("isDark", isDark);
+    });
+  }
+
+  @override
+  void initState() {
+
+    super.initState();
+
+    isDark =
+        SharedPreferencesHelper
+            .getBool("isDark") ?? false;
+  }
+
+  @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
+
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(toggleTheme: () {  },),
+
+      theme:
+      isDark
+          ? ThemeData.dark()
+          : ThemeData.light(),
+
+      home: LoginScreen(
+        toggleTheme: toggleTheme,
+      ),
     );
   }
 }
